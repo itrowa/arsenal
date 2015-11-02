89c38ab0beba9af16b6af4e20e9e53a8b0360b59
Trowa <acesolo@gmail.com>
2015-11-02 15:19:05 +0800

    start learning processing

--- mklink.bat
+++ mklink.bat
@@ -4,4 +4,5 @@ mklink /j "D:\Desk-Sync\Github-arsenal\arsenal\sicp" "D:\Desk-Sync\822 SICP"
 mklink /j "D:\Desk-Sync\Github-arsenal\arsenal\CS61A" "D:\Desk-Sync\821 CS61A"
 mklink /j "D:\Desk-Sync\Github-arsenal\arsenal\scheme" "D:\Desk-Sync\822.1 scheme"
 mklink /j "D:\Desk-Sync\Github-arsenal\arsenal\tls" "D:\Desk-Sync\803 tls-note"
+mklink /j "D:\Desk-Sync\Github-arsenal\arsenal\Noc" "D:\Desk-Sync\827 NoC"
 
