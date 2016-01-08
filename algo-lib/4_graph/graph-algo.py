20f68ed155756315152ce453965b3a7819e8a949
Trowa <acesolo@gmail.com>
2016-01-08 16:58:18 +0800

    update
    更新了有向图拓扑搜索和从算法书上读取图的数据的代码

--- algo-lib/4_graph/graph-algo.py
+++ algo-lib/4_graph/graph-algo.py
@@ -30,6 +30,37 @@ g3 = { 0: [6, 2, 1, 5],
        12: [11, 9]
        }
 
+# a sample directed graph in algs4 book for example.
+tinyDG = { 0: [5, 1],
+           1: [],
+           2: [0, 3],
+           3: [5, 2],
+           4: [3, 2],
+           5: [4],
+           6: [9, 4, 0],
+           7: [6, 8],
+           8: [7, 9],
+           9: [11, 10],
+           10: [12],
+           11: [4, 12],
+           12: [9]
+    
+}
+
+tinyDAG = {0: [6, 1, 5], 
+           1: [],
+           2: [3, 0], 
+           3: [5], 
+           4: [],
+           5: [4], 
+           6: [4, 9], 
+           7: [6], 
+           8: [7], 
+           9: [12, 10, 11], 
+           10: [],
+           11: [12],
+           12: []}
+    
 ###############################################################
 
 def v(graph):
@@ -50,6 +81,7 @@ def dfs(graph, start, visited=None):
     return visited
 
 dfs(g1, 0)
+dfs(tinyDG, 2) # 2, 0, 5, 4, 3, 1
 
 def bfs(graph, start):
     """ 找到图graph中所有和start点连接的点. 广度优先. 迭代的实现.
@@ -77,10 +109,8 @@ def bfs_alt(graph, start):
             visited.append(vertex)
         for next in graph[vertex]:
             queue.append(next)
-
-
-bfs(g1, 0)
-bfs(g2, 'A')
+bfs (g1, 0)
+bfs (g2, 'A')
 
 def bfs_path(graph, start, goal):
     """ 求图graph中从start点到goal点的路径.
@@ -111,6 +141,57 @@ def bfs_path(graph, start, goal):
 bfs_path(g1, 0, 5) # [0, 5]
 bfs_path(g1, 0, 3) # [0, 2, 3]
 
+# def get_cycle(graph, v):
+#     """ 求有向图的环的所有顶点. 如果返回False则说明这是有向无环图(DAG)
+#         @todo! 没完成!!!
+#     """
+#     visited = []
+#     parents = {}
+#     stack = []
+#     cycle = []
+
+#     def dfs_mark(graph, s):
+#         """ 在dfs搜索中检查环的存在, 并给出parents mapping
+#         """
+#         stack.append(s)
+#         while stack:
+#             v = stack.pop()
+#             if v in stack: # 这说明遇到环了
+#                 # ss = 
+#             if v not in visited:
+#                 visited.append(v)
+#             for next in graph[v]:
+#                 if next not in visited:
+#                     stack.append(next)
+#                     parents[next] = v
+#     return dfs_mark(graph, v)
+
+def topological_sort(graph):
+    reverse_post = [] # 一个栈
+    visited = []
+
+    def dfs(graph, start):
+        visited.append(start)
+        print(start)
+        # print("visited:")
+        # print(visited)
+        for v in graph[start]:
+            if v not in visited:
+                dfs(graph, v)
+        reverse_post.append(start)
+        # print("reverse_post:")
+        # print(reverse_post)
+
+    for vertice in list(graph.keys()):
+        if vertice not in visited:
+            dfs(graph, vertice)
+
+    reverse_post.reverse()
+    return reverse_post   #将栈反向才是逆后序的点列
+
+topological_sort(tinyDAG)
+# [8, 7, 2, 3, 0, 5, 1, 6, 9, 11, 10, 12, 4]
+# 注意: 排序结果不唯一.
 
 def cc(graph):
     """ 计算一幅图的连通分量
@@ -120,6 +201,8 @@ def cc(graph):
     ci = 0
 
     def dfs_mark(graph, s):
+        """ 在dfs中标记连通分量
+        """
         visited.append(s)
         components[ci].append(s)
         for next in graph[s]:
--- algo-lib/4_graph/readdg.py
+++ algo-lib/4_graph/readdg.py
@@ -12,14 +12,45 @@ raw_edges = [line.split() for line in sys.stdin]
 
 # 把元素从string类型转换为int
 for pair in raw_edges:
+    if not pair: # 如果pair是[]
+        raw_edges.remove([])
     for i in range(len(pair)):
         pair[i] = int(pair[i])
 
+print(raw_edges)
 # print 
-print(v_cnt)
-print(e_cnt)
-
+print(str(v_cnt) + str(e_cnt))
 for edge in raw_edges:
     s = str(edge[0]) + " → " + str(edge[1])
     print(s) 
+
+# 生成graph
+graph = {}
+for edge in raw_edges:
+    if edge[0] not in graph:
+        graph[edge[0]] = []
+        graph[edge[0]].append(edge[1])
+    else:
+        graph[edge[0]].append(edge[1])
+print(graph)
+
+# 一个simGraph类的草稿
+class simpleGraph:
+    """ 用字典来表示邻接表. 邻接表来表示一个图.
+        创建好的图是这样的:
+        g.data= 
+        {'A': ['B', 'C'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F'],
+         'D': ['B'],
+         'E': ['B', 'F'],
+         'F': ['C', 'E']}
+    """
+    def __init__(self):
+        data = {}
+
+    def addEdge(self, pair):
+        """ 添加一条边, pair是一个有两个顶点组成的列表.
+        """
+        data[pair[0]] = pair[1]
 
